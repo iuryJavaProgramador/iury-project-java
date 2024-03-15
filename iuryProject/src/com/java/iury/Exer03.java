b337321dd698576bfd423225d010ea120059b65e
iury Rodrigues de Godoy <148925676+iuryJavaProgramador@users.noreply.github.com>
2024-03-14 22:39:32 -0300

    Update Exer03.java

--- iuryProject/src/com/java/iury/Exer03.java
+++ iuryProject/src/com/java/iury/Exer03.java
@@ -7,12 +7,11 @@ public class Exer03 {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		int calculo = 0;
+		
 		int n1;
 		int n2;
 		int sum;
 		int mult;
-		boolean even, odd;
 		
 		System.out.println("Enter odd numbers between 0 and 30");
 		
@@ -31,14 +30,6 @@ public class Exer03 {
 		
 		} else if (n1 > 30 & n2 > 30 ) {
 			 System.out.println("The numbers are greater than 30.");
-		} 
-		
-		mult = n1 * n2;
-		if (n1 <= 30 & n2 <= 30) {
-			System.out.println("The multiplication of even numbers between 0 and 30 is: " + mult + n1%2 + n2%2);
-		}
-		
-	
+		} 	
 	}
-
 }
